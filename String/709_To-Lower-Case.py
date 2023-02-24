c0ef6e794ebabcbf6954ef6fb6f6fd5145621a8d
andang <danghieuan@gmail.com>
2023-02-24 17:14:00 +0700

    update 709 solution status

--- String/709_To-Lower-Case.py
+++ String/709_To-Lower-Case.py
@@ -1,6 +1,6 @@
-# Pending ASCII solution
+# Pending ASCII solution 
 class Solution:
   def toLowerCase(self, s: str) -> str:
         """Given a string s,
     return the string after replacing every uppercase letter with the same lowercase letter. """
-        return s.lower()
+        return s.lower() # python library
